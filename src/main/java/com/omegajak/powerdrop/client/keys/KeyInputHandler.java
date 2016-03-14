043faa7ace5f6a245116bdae431798940750175b
OmegaJak <jak.kruger@gmail.com>
2016-03-14 15:21:51 -0500

    Fixed fov zooming in/out when fast-dropping lots of things

--- src/main/java/com/omegajak/powerdrop/client/keys/KeyInputHandler.java
+++ src/main/java/com/omegajak/powerdrop/client/keys/KeyInputHandler.java
@@ -19,6 +19,7 @@ public class KeyInputHandler {
 	
 	boolean previousQState = false;
 	long keyDownTime = 0;
+	long finalChargeTime = 0;
 	boolean wasCtrlDown = false;
 	
 	double lastChargeFactor = 0;
@@ -43,34 +44,39 @@ public class KeyInputHandler {
 	
 	@SubscribeEvent
 	public void onClientTickEvent(TickEvent.ClientTickEvent event) {
-		GameSettings settings = Minecraft.getMinecraft().gameSettings; // Just so I don't have to keep typing this
-		if (KeyBindings.drop.getIsKeyPressed()) {
-			double chargeFactor = convertChargeTimeToFactor(System.currentTimeMillis() - keyDownTime);
-			if (chargeFactor == 4.0 && lastChargeFactor != 4.0) { // If we just now reached the maximum power
-				EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
-				player.addChatMessage(new ChatComponentText("MAXIMUM POWER ACHIEVED").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GOLD)));
-				lastChargeFactor = 4.0; // So we don't keep spamming the max message
-			}
+		if (KeyBindings.drop.getIsKeyPressed() || isFOVResetting) {
+			GameSettings settings = Minecraft.getMinecraft().gameSettings; // Just so I don't have to keep typing this
 			
-			chargeFactor -= 1.0;
-			Minecraft.getMinecraft().gameSettings.fovSetting /= fovMultiplier; // Reset so we don't have cumulative multiplication (can get out of hand very fast)
-			fovMultiplier = 1.0F + (float)(chargeFactor / 8.0);
-			
-			// Max chargeFactor is 4.0, so max fovMultiplier is 1.5
-			if (settings.fovSetting * fovMultiplier < originalFOV * 1.5) // If we haven't reached the max fov yet
-				settings.fovSetting *= fovMultiplier;
-			else
-				settings.fovSetting = originalFOV * 1.5F; // Hard cap at multiplying by 1.5F
-		} else if (isFOVResetting) {			
-			float diff = settings.fovSetting - originalFOV;
-			
-			if (diff / 8.0F < 0.001F) { // Gotta stop at some point
-				settings.fovSetting = originalFOV; // Reset
-				isFOVResetting = false;
-				fovMultiplier = 1.0F;
-			} else {
-				settings.fovSetting -= diff / 3.0F; // Produces a nice, smooth curve for resetting
-				fovMultiplier = originalFOV / settings.fovSetting; // This is necessary for when the reset isn't allowed to finish
+			// The second half of this statement is half trying to predict whether the next drop will be short or long so as to not make the FOV go in/out
+			// 		and half making sure that if a long drop is attempted after short drops, the fov will catch up
+			if (KeyBindings.drop.getIsKeyPressed() && (finalChargeTime > 120 || System.currentTimeMillis() - keyDownTime > 150)) {
+				double chargeFactor = convertChargeTimeToFactor(System.currentTimeMillis() - keyDownTime);
+				if (chargeFactor == 4.0 && lastChargeFactor != 4.0) { // If we just now reached the maximum power
+					EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
+					player.addChatMessage(new ChatComponentText("MAXIMUM POWER ACHIEVED").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GOLD)));
+					lastChargeFactor = 4.0; // So we don't keep spamming the max message
+				}
+				
+				chargeFactor -= 1.0;
+				Minecraft.getMinecraft().gameSettings.fovSetting /= fovMultiplier; // Reset so we don't have cumulative multiplication (can get out of hand very fast)
+				fovMultiplier = 1.0F + (float)(chargeFactor / 8.0);
+				
+				// Max chargeFactor is 4.0, so max fovMultiplier is 1.5
+				if (settings.fovSetting * fovMultiplier < originalFOV * 1.5) // If we haven't reached the max fov yet
+					settings.fovSetting *= fovMultiplier;
+				else
+					settings.fovSetting = originalFOV * 1.5F; // Hard cap at multiplying by 1.5F
+			} else if (isFOVResetting) {			
+				float diff = settings.fovSetting - originalFOV;
+				
+				if (diff / 8.0F < 0.001F) { // Gotta stop at some point
+					settings.fovSetting = originalFOV; // Reset
+					isFOVResetting = false;
+					fovMultiplier = 1.0F;
+				} else {
+					settings.fovSetting -= diff / 3.0F; // Produces a nice, smooth curve for resetting
+					fovMultiplier = originalFOV / settings.fovSetting; // This is necessary for when the reset isn't allowed to finish
+				}
 			}
 		}
 	}
@@ -87,9 +93,10 @@ public class KeyInputHandler {
 				originalFOV = Minecraft.getMinecraft().gameSettings.fovSetting;
 			
 		} else if (!KeyBindings.drop.getIsKeyPressed() && !KeyBindings.drop.isPressed() && previousQState) {
-			long timeElapsed = System.currentTimeMillis() - keyDownTime;
+			finalChargeTime = System.currentTimeMillis() - keyDownTime;
+			System.out.println(finalChargeTime);
 			
-			PowerDrop.network.sendToServer(new DropMessage(convertChargeTimeToFactor(timeElapsed), wasCtrlDown));
+			PowerDrop.network.sendToServer(new DropMessage(convertChargeTimeToFactor(finalChargeTime), wasCtrlDown));
 			
 			wasCtrlDown = false;
 			
