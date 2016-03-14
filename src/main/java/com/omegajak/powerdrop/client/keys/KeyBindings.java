33d18f4554aa83dc1c66f55fbbc780d0c5f7386d
OmegaJak <jak.kruger@gmail.com>
2016-03-14 16:58:21 -0500

    Localized keybind

--- src/main/java/com/omegajak/powerdrop/client/keys/KeyBindings.java
+++ src/main/java/com/omegajak/powerdrop/client/keys/KeyBindings.java
@@ -2,12 +2,13 @@ package com.omegajak.powerdrop.client.keys;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.StatCollector;
 
 public class KeyBindings {
 	public static KeyBinding drop;
 	
 	public static void init() {
-		drop = new KeyBinding("key.powerDrop", 16, "key.categories.gameplay");
+		drop = new KeyBinding(StatCollector.translateToLocal("key.powerdrop"), 16, "key.categories.gameplay");
 		
 		ClientRegistry.registerKeyBinding(drop);
 	}
