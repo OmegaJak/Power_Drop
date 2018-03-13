fda698949c5e53694b0dcef7d120a8a5634c92c6
OmegaJak <jak.kruger@gmail.com>
2018-03-13 13:50:02 -0500

    More keybinding tweaks to minimize conflict with default drop

--- src/main/java/com/omegajak/powerdrop/client/keys/KeyBindings.java
+++ src/main/java/com/omegajak/powerdrop/client/keys/KeyBindings.java
@@ -2,6 +2,7 @@ package com.omegajak.powerdrop.client.keys;
 
 import org.lwjgl.input.Keyboard;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 
@@ -11,5 +12,10 @@ public class KeyBindings {
 	public static void init() {
 		drop = new KeyBinding("key.powerdrop", Keyboard.KEY_Q, "key.categories.gameplay");
 		ClientRegistry.registerKeyBinding(drop);
+	}
+	
+	public static void postInit() {
+		if (drop.conflicts(Minecraft.getMinecraft().gameSettings.keyBindDrop))
+			Minecraft.getMinecraft().gameSettings.keyBindDrop.setKeyCode(Keyboard.KEY_NONE);
 	}
 }
--- src/main/java/com/omegajak/powerdrop/proxies/ClientProxy.java
+++ src/main/java/com/omegajak/powerdrop/proxies/ClientProxy.java
@@ -5,11 +5,21 @@ import com.omegajak.powerdrop.client.keys.KeyInputHandler;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
 	@Override
 	public void init(FMLInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(new KeyInputHandler());
 		KeyBindings.init();
+		
+		super.init(event);
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent e) {
+		KeyBindings.postInit();
+		
+		super.postInit(e);
 	}
 }
