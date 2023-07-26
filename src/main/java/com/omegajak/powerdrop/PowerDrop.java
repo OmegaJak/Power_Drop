dcec5df40b3dc010ef5a702fed13e4faa7122e71
OmegaJak <jak.kruger@gmail.com>
2023-07-26 17:19:38 -0500

    Cleanup main

--- src/main/java/com/omegajak/powerdrop/PowerDrop.java
+++ src/main/java/com/omegajak/powerdrop/PowerDrop.java
@@ -3,12 +3,8 @@ package com.omegajak.powerdrop;
 import com.mojang.logging.LogUtils;
 import com.omegajak.powerdrop.client.KeybindHandler;
 import com.omegajak.powerdrop.network.PowerDropPacketHandler;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -17,7 +13,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(PowerDrop.MODID)
 public class PowerDrop
 {
@@ -47,24 +42,5 @@ public class PowerDrop
 
     private void clientSetup(FMLClientSetupEvent event) {
         KeybindHandler.register(event);
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        // Do something when the server starts
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents
-    {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent)
-        {
-            // Register a new block here
-        }
     }
 }
